45aa33bfd22897ab1e758ca2ef0408bad235530a
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-03-19 19:31:39 +0200

    posts with admin panel

---
+++ admin/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('is_admin')->group(function () {
+        Route::resource('categories', CategoryController::class);
+        Route::resource('posts', PostController::class);
+    });
+});
+
+
+require __DIR__ . '/auth.php';
---
+++ posts/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/about', 'about')->name('about');
+
+Route::get('posts/{post}', [PostController::class, 'show'])->name('post.show');
+
